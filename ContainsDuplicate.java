8ce43866c9c740e66912eedf118d5c88984a2336
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-04-26 10:11:28 +0530

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,1 +1,31 @@
-
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // int count=0;
+        // for(int i=0;i<nums.length-1;i++)
+        // {
+        //     count=1;
+        //     for(int j=i+1;j<nums.length;j++)
+        //     {
+        //         if(nums[i]==nums[j])
+        //         {
+        //             count++;
+        //             if(count>=2)
+        //             {
+        //                 return true;
+        //             }
+        //         }
+        //     }
+        // } 
+        // return false;
+        HashSet<Integer> seen=new HashSet<>();
+        for(int num:nums)
+        {
+            if(seen.contains(num))
+            {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
