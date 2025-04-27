625c281813e7f0728fc901ff7cb2a402624e0874
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-04-27 10:53:07 +0530

    Update ProductMinusSum.java

--- ProductMinusSum.java
+++ ProductMinusSum.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int sum=0, product=1;
+        while(n>0)
+        {
+            int lastDigit=n%10;
+            sum+=lastDigit;
+            product*=lastDigit;
+            n=n/10;
+        }
+        int x;
+        x=product-sum;
+        return x;
+    }
+}
