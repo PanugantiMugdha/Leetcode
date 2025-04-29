3272c4d842c3af2dad6a0238195449880f9c41f8
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-04-29 16:06:24 +0530

    Update Sqrt(x).java

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -3,3 +3,8 @@
 // You must not use any built-in exponent function or operator.
 
 // For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+class Solution {
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+}
