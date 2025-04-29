dc58598a9de961995be205f0e63b534254982b17
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-04-29 16:04:50 +0530

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -8,3 +8,15 @@
 
 // Input: nums = [1,3,5,6], target = 5
 // Output: 2
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>=target)
+            {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
