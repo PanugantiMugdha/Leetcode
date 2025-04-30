be2b22169e2e112dbbda265b958d15a6c0a53429
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-04-30 12:43:44 +0530

    Update PowerOfTwo.java

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -9,3 +9,15 @@
 // Input: n = 1
 // Output: true
 // Explanation: 20 = 1
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        for(int i=0;i<=50;i++)
+        {
+            if(Math.pow(2,i)==n)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
