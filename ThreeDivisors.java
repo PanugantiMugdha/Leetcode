30f59d4ebcc22d64cf8b56327518dd75c4e79afa
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-01 13:18:27 +0530

    Update ThreeDivisors.java

--- ThreeDivisors.java
+++ ThreeDivisors.java
@@ -9,3 +9,20 @@
 // Input: n = 2
 // Output: false
 // Explantion: 2 has only two divisors: 1 and 2.
+class Solution {
+    public boolean isThree(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==3)
+        {
+            return true;
+        }
+        return false;
+    }
+}
