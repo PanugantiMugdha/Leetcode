dcc9bb07bf5ca4c6996a1a55f4ae925535e7c030
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-03 19:56:17 +0530

    Update SumMultiples.java

--- SumMultiples.java
+++ SumMultiples.java
@@ -1,1 +1,11 @@
+Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
 
+Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+
+ 
+
+Example 1:
+
+Input: n = 7
+Output: 21
+Explanation: Numbers in the range [1, 7] that are divisible by 3, 5, or 7 are 3, 5, 6, 7. The sum of these numbers is 21.
