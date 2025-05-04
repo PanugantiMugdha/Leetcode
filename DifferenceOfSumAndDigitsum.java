c3dbb8b6f1277bb5993aed98866ede63214d1947
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-04 08:10:06 +0530

    Update DifferenceOfSumAndDigitsum.java

--- DifferenceOfSumAndDigitsum.java
+++ DifferenceOfSumAndDigitsum.java
@@ -10,3 +10,24 @@
 // The element sum of nums is 1 + 15 + 6 + 3 = 25.
 // The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
 // The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+class Solution {
+    public int differenceOfSum(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            sum+=nums[i];
+        }
+        int digitSum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            while(nums[i]>0)
+            {
+                int ld;
+                ld=nums[i]%10;
+                digitSum+=ld;
+                nums[i]=nums[i]/10;
+            }
+        }
+        return (sum-digitSum);
+    }
+}
