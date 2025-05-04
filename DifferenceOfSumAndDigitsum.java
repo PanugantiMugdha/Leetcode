8d43956d348054cd9dde23b002687758fdc253e1
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-04 08:09:40 +0530

    Update DifferenceOfSumAndDigitsum.java

--- DifferenceOfSumAndDigitsum.java
+++ DifferenceOfSumAndDigitsum.java
@@ -1,12 +1,9 @@
 You are given a positive integer array nums.
-
 The element sum is the sum of all the elements in nums.
 The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
 Return the absolute difference between the element sum and digit sum of nums.
-
 Note that the absolute difference between two integers x and y is defined as |x - y|.
 Example 1:
-
 Input: nums = [1,15,6,3]
 Output: 9
 Explanation: 
