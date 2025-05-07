7bfb30618d7fdd8a7084e52e1874b391c07269b6
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-07 20:25:47 +0530

    Update AddDigits.java

--- AddDigits.java
+++ AddDigits.java
@@ -8,3 +8,16 @@
 // 38 --> 3 + 8 --> 11
 // 11 --> 1 + 1 --> 2 
 // Since 2 has only one digit, return it.
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
