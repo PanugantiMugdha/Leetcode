c44defd9f1693f7f82f9030e9b17848cd4ea742f
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-08 20:37:23 +0530

    Update DivisibleAndNonDivisibleSumDifference.java

--- DivisibleAndNonDivisibleSumDifference.java
+++ DivisibleAndNonDivisibleSumDifference.java
@@ -10,3 +10,23 @@
 // - Integers in the range [1, 10] that are not divisible by 3 are [1,2,4,5,7,8,10], num1 is the sum of those integers = 37.
 // - Integers in the range [1, 10] that are divisible by 3 are [3,6,9], num2 is the sum of those integers = 18.
 // We return 37 - 18 = 19 as the answer.
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int s=0;
+        int t=0;
+        int diff=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%m!=0)
+            {
+                s+=i;
+            }
+            if(i%m==0)
+            {
+                t+=i;
+            }
+            diff=s-t;
+        }
+        return diff;
+    }
+}
