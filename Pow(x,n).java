3b7fbf565adbbfde52291a5cc1d081a01a4a959a
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-13 07:03:23 +0530

    Update Pow(x,n).java

--- Pow(x,n).java
+++ Pow(x,n).java
@@ -1,8 +1,8 @@
-Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+// Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
  
 
-Example 1:
+// Example 1:
 
-Input: x = 2.00000, n = 10
-Output: 1024.00000
+// Input: x = 2.00000, n = 10
+// Output: 1024.00000
