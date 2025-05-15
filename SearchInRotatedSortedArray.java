09c99bf8088bd9c352560ba1b89d9707450dc3b8
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-05-15 21:10:34 +0530

    Update SearchInRotatedSortedArray.java

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -10,3 +10,25 @@
 
 // Input: nums = [4,5,6,7,0,1,2], target = 0
 // Output: 4
+class Solution {
+    public int search(int[] nums, int target) {
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]==target)
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        int i=0;
+        while(i<nums.length)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+}
