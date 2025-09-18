a7a1258b04f449753061f71c52965d7574dd8ea4
Panuganti Mugdha <144349731+PanugantiMugdha@users.noreply.github.com>
2025-09-18 17:45:43 +0530

    Update Better.java

--- Arrays 1/2965. Find Missing and Repeated Values/Better.java
+++ Arrays 1/2965. Find Missing and Repeated Values/Better.java
@@ -1,1 +1,35 @@
-
+public class Solution
+{
+    public static int[] findMissingAndRepeatedValues(int[][] grid)
+    {
+        int n=grid.length;
+        int M=n*n;
+        int repeated=0;
+        int missing=0;
+        boolean[] seen=new boolean[M+1];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int v=grid[i][j];
+                if(seen[v]==true)
+                {
+                    repeated=v;
+                }
+                else
+                {
+                    seen[v]=true;
+                }
+            }
+        }
+        for(int v=1;v<=M;v++)
+        {
+            if(seen[v]==false)
+            {
+                missing=v;
+                break;
+            }
+        }
+        return new int[] {repeated,missing};
+    }
+}
